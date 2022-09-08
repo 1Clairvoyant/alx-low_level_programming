044262a8f0c2feda61a8354f3c4afb7f35d04d7c
Joseph Olojede <janetogunkua@gmail.com>
2022-09-08 14:32:39 -0700

    Task 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+/**
+ * main - print the string in the function
+ *
+ * description: using the main function
+ * this prints "Programming is like building a multilingual puzzle 
+ * return: 0
+ */
+int main(void) 
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+       return (0);
+}
+
