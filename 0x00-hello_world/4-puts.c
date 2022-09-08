8f41aa969887ddbdc5d6606bbcccaa6c6fd48287
Joseph Olojede <janetogunkua@gmail.com>
2022-09-08 14:14:50 -0700

    Task Omo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+/**
+ * main - print the string in the put function
+ *
+ * description: use the main function 
+ * to print 'Programming is like building a multilingual puzzle'
+ * return; 0
+ */
+int main(void)
+{ 
+	puts("\" "Programming is like building a multilingual puzzle");
+	return (0);
+}
+
