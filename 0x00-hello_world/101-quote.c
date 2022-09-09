f69fff756b4f35d63bd4b8a8e2ef4a70607e8d72
Joseph Olojede <111068179+1Clairvoyant@users.noreply.github.com>
2022-09-09 03:01:37 +0100

    Rest

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * return: 1 (success)
+ * Return:1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
