19ed2890769f5c8718fc2c91fe9531a2e5dae3e0
Joseph Olojede <janetogunkua@gmail.com>
2022-09-13 13:09:56 -0700

    prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+  *print_alpha - prints the lowercase alphabets
+  */
+void print_alpha(void)
+
+{
+	int j;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+	{
+		_putchar(j);
+	}
+	_putchar ('\n');
+
+}
