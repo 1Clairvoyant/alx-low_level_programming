1c778b3989da3919b45bd67c3e57460b7677e5d3
Joseph Olojede <111068179+1Clairvoyant@users.noreply.github.com>
2022-09-27 23:09:06 +0100

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,31 @@
-#include "main.h"
+include "main.h"
+
 /**
-  * *_strspn - return the number of occurent of a string
-  * @s: string to check
-  * @accept: character to match
-  * Return: int
+ * *_strspn - return the number of occurent of a string
+ * @s: string to check
+ * @accept: character to match
+ * Return: int
  */
+
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int bytes = 0;
-	int a;
+	int i, j, cmpt = 0;
 
-	while (*s)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		for (a = 0; accept[a]; a++)
+		for (j = 0; accept[j] > '\0'; j++)
 		{
-			if (*s == accept[a])
+			if (s[i] == accept[j])
 			{
-				bytes++;
+				cmpt++;
 				break;
 			}
-			else if (accept[a + 1] == '\0')
-				return (bytes);
 		}
-		s++;
+		if (accept[j] == '\0')
+		{
+			break;
+		}
 	}
-	return (bytes);
+	return (cmpt);
 }
