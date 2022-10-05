61bab3421b49ecf9faee620bdea2c483f333698c
Joseph Olojede <janetogunkua@gmail.com>
2022-10-04 21:34:34 -0700

    advanced task

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,40 +9,45 @@
  * Return: char value
  */
 
+int _strlen(char *s)
+{
+int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
+}
+
+/**
+ * *argstostr - description
+ * @ac: int
+ * @av: arguments
+ * Return: string
+ */
+
 char *argstostr(int ac, char **av)
 {
-	int size;
-	char *s;
-	int i;
-	int j;
-	int k;
+int i = 0, nc = 0, j = 0, cmpt = 0;
+char *s;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+if (ac == 0 || av == NULL)
+	return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			size++;
-	}
+for (; i < ac; i++, nc++)
+	nc += _strlen(av[i]);
 
-	size += (ac + 1);
-	s = malloc(sizeof(char) * size);
-	if (s == NULL)
-		return (NULL);
-	k = 0;
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			s[k] = av[i][j];
-			k++;
-		}
-		s[k] = '\n';
-		k++;
-	}
+s = malloc(sizeof(char) * nc + 1);
+if (s == 0)
+	return (NULL);
 
-	s[k] = '\0';
+for (i = 0; i < ac; i++)
+{
+	for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+		s[cmpt] = av[i][j];
 
-	return (s);
+	s[cmpt] = '\n';
+	cmpt++;
+}
+s[cmpt] = '\0';
+
+return (s);
 }
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,89 +1,111 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * helper - helps function
- * @word: wordcount
- * @len: length
- * @str: string to go through
- * @s: array you are assigning
- * Return: char value
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
  */
 
-char **helper(int word, int len, char *str, char **s)
-{
-	int i, k, j;
 
-	j = 0;
-	for (i = 0; i < word; i++)
-	{
-		k = 0;
-		for (; j < len; j++)
-		{
-			if (str[0] != ' ' || str[j] != ' ')
-			{
-				s[i][k] = str[j];
-				k++;
-			}
-			if (j != 0 && str[j] == ' ' && str[j - 1] != ' ')
-			{
-				j++;
-				break;
-			}
-		}
-		s[i][k + 1] = '\0';
-	}
-	s[word + 1] = NULL;
-	return (s);
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++);
+	
+	return (size);
 }
 
 /**
- * strtow - string to words
- * @str: string to check
- * Return: return char value
+ * *str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer
  */
 
+char *str_addChar(char *str, char c)
+{
+	int size, i;
+	char *m;
+
+	size = _strlen(str);
+
+	m = malloc((size + 1) * sizeof(char) + 1);
+	if (m == 0)
+		return (0);
+
+	for (i = 0; i <= size; i++)
+		m[i] = str[i];
+
+	m[i + 1] = c;
+	m[i + 2] = '\0';
+	return (m);
+}
+
+
+/**
+ * *nbr_spaces - return the number of occurent of a string
+ * @s: string to check
+ * Return: int
+ */
+
+unsigned int nbr_spaces(char *s)
+{
+	int i, cmpt = 0;
+
+	for (i = 0; s[i + 1] != '\0'; i++)
+	{
+		if (s[i]  == ' ' && s[i + 1] != ' ')
+			cmpt++;
+	}
+
+	return (cmpt + 1);
+}
+
+
+/**
+  *strtow - split a sentence into multiple words.
+  *@str: the string passed as argument.
+  *Return: tokens
+  */
 char **strtow(char *str)
 {
-	int len, i, j, size, k, word;
-	char **s;
+	int i;
+	int spaces = nbr_spaces(str);
+	char **tokens = NULL;
+	malloc(sizeof(char *) * (spaces));
 
-	if (str == NULL)
-		return (NULL);
-	len = 0;
-	word = 0;
-	while (str[len] != '\0')
+	char *token;
+	int checkingSpace = 0;
+	int word = 0;
+
+	if (!tokens)
 	{
-		if (str[0] != ' ')
-		word++;
-		if (str[len] != ' ' && str[len - 1] == ' ' && len != 0)
-			word++;
-		len++;
+		printf("Failed");
+		return (0);
 	}
-	s = (char **)malloc(sizeof(char *) * word + 1);
-	if (s == NULL)
-		return (NULL);
-	j = 0;
-	for (i = 0; i < word; i++)
+
+	printf("looping");
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		size = 0;
-		for (; j < len; j++)
+		if (str[i] == ' ')
 		{
-			if (str[0] != ' ' || str[j] != ' ')
-				size++;
-			if (str[j] == ' ' && size > 0)
-				break;
+			if (checkingSpace == 0)
+			{
+				word++;
+				checkingSpace = 1;
+			}
 		}
-		printf("%d\n", size);
-		s[i] = (char *)malloc(sizeof(char) * size + 1);
-		if (s[i] == NULL)
+		else
 		{
-			for (k = i - 1; k >= 0; k--)
-				free(s[k]);
-			free(s);
+			printf("1");
+			token = tokens[word];
+			free(tokens[word]);
+			str_addChar(token, str[i]);
+			checkingSpace = 0;
 		}
 	}
-	s = helper(word, len, str, s);
-	return (s);
+	tokens[i] = NULL;
+
+	return (tokens);
 }
